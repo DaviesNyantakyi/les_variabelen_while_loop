936e93ba6d08e2ca36b45a5b62c964a74c17ed9e
DaviesNyantakyi <davies.nyantakyi@outlook.com>
2024-08-03 02:03:08 +0200

    while loop

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Aantalflessen
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            int aantalFlessen = 0;
+            int aantalBakken = 0;
+
+            Console.Write("Geef het aantal flesjes in: ");
+             aantalFlessen = int.Parse(Console.ReadLine());
+
+            while (aantalFlessen > 0) {
+
+                aantalBakken++;
+                aantalFlessen -= 24;
+            }
+            Console.WriteLine($"Aantal bakken nodig= {aantalBakken}");
+        }
+    }
+}
